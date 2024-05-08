57b2b3b466212bfdd652cc33b29d216c0b8de82d
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-08 03:09:33 +0100

    update

---
+++ FrontEnd/Events/Handlers.cs
@@ -0,0 +1,34 @@
+﻿
+namespace FrontEnd.Events
+{
+    public delegate void AfterUpdateEventHandler(object? sender, AfterUpdateArgs e);
+    public delegate void BeforeUpdateEventHandler(object? sender, BeforeUpdateArgs e);
+
+    public abstract class UpdateArgs(string propertyName) : EventArgs
+    {
+        public readonly string propertyName = propertyName;
+
+        /// <summary>
+        /// Check the if current property name is equal to the given value. 
+        /// </summary>
+        /// <param name="value">the name of a Property</param>
+        /// <returns>bool if the current property name is equal to the given value</returns>
+        public bool Is(string value) => propertyName.Equals(value);
+
+    }
+
+    public class AfterUpdateArgs(object? value, object? backProperty, string propertyName) : UpdateArgs(propertyName)
+    {
+        public object? value = value;
+        public readonly object? backProperty = backProperty;
+    }
+
+    public class BeforeUpdateArgs(object? value, object? backProperty, string propertyName) : AfterUpdateArgs(value, backProperty, propertyName)
+    {
+        /// <summary>
+        /// This property is used to notify if an update can be processed or not.
+        /// </summary>
+        /// <value>True is the property can be updated,</value>
+        public bool Cancel { get; set; } = false;
+    }
+}
