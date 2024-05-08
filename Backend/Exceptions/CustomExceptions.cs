2e7a7ae28b348ff20f24c55c75752cc668d0e288
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-08 21:16:43 +0100

    update

--- Backend/Exceptions/CustomExceptions.cs
+++ Backend/Exceptions/CustomExceptions.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Backend.Exceptions
 {
-    public class NoModelException : Exception
-    { 
-        public NoModelException() : base("No Model is set") { }
-    }
+    public class NoModelException() : Exception("No Model is set") { }
 }
