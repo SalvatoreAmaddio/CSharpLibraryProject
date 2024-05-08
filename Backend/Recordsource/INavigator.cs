6414f60b3446a5371e8da1b7cf079f85ef17ad37
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-08 21:10:46 +0100

    update

--- Backend/Recordsource/INavigator.cs
+++ Backend/Recordsource/INavigator.cs
@@ -6,7 +6,7 @@ namespace Backend.Recordsource
     /// This interface extends the IEnumerator&lt;ISQLModel&gt; 
     /// and adds extra functionalities.
     /// For instance, this enumerator can move up and down the IEnumerable.
-    /// This interface is meant for dealing with IEnumerable&lt;ISQLModel&gt; only.
+    /// This interface is meant for dealing with IEnumerable&lt;<see cref="ISQLModel"/>&gt; only.
     /// </summary>
     public interface INavigator : IEnumerator<ISQLModel?>
     {
