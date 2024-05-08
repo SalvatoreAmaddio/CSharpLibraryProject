f65e7e7cc8d2970dfac90da4de08222c21ea0126
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-08 21:39:32 +0100

    update documentation

--- Backend/Recordsource/INavigator.cs
+++ Backend/Recordsource/INavigator.cs
@@ -4,14 +4,14 @@ namespace Backend.Recordsource
 {
     /// <summary>
     /// This interface extends the IEnumerator&lt;ISQLModel&gt; 
-    /// and adds extra functionalities.
+    /// to add extra functionalities.
     /// For instance, this enumerator can move up and down the IEnumerable.
     /// This interface is meant for dealing with IEnumerable&lt;<see cref="ISQLModel"/>&gt; only.
     /// </summary>
     public interface INavigator : IEnumerator<ISQLModel?>
     {
         /// <summary>
-        /// This boolean indicates if there are no records at all. 
+        /// Tells if there are no records at all. 
         /// This property returns false if <see cref="Index"/> is pointing to a New Record.<para/>
         /// see also: <seealso cref="IsNewRecord"/>
         /// </summary>
@@ -24,93 +24,88 @@ namespace Backend.Recordsource
         public int Index { get; set; }
 
         /// <summary>
-        /// It tells if the Array is empty.
+        /// Tells if the collection is empty.
         /// </summary>
-        /// <value>True if the Array is empty</value>
         public bool IsEmpty { get; }
 
         /// <summary>
-        /// This boolean indicates if <see cref="Index"/> is at the beggining of the Array.
+        /// Tells if <see cref="Index"/> is at the beggining of the Collection.
         /// </summary>
-        /// <value>True if <see cref="Index"/> is at the first position. i.e. 0</value>
         public bool BOF { get; }
 
         /// <summary>
-        /// This boolean indicates if <see cref="Index"/> is at the end of the Array. Therefore, it reached the last available position.
+        /// Tells if <see cref="Index"/> is at the end of the Collection.
         /// </summary>
-        /// <value>True if <see cref="Index"/> is at the last position.</value>
         public bool EOF { get; }
 
         /// <summary>
-        /// This boolean indicates if Enumerator is pointing to a New Record.
+        /// Tells if Enumerator is pointing at a New Record.
         /// </summary>
-        /// <value>True if it is pointing a New Record</value>
         public bool IsNewRecord { get; }
 
         /// <summary>
-        /// Get the one-based position within the Array.
+        /// Get the one-based position within the Collection.
         /// see <see cref="Index"/>.
         /// <example>
         ///  <para>For example: array[0] is record number 1, array[1] is record number 2 ...</para>
         /// </example>
         /// </summary>
-        /// <value>The Record's Number.</value>
         public int RecNum { get; }
 
         /// <summary>
-        /// It moves the Enumerator to the previous position.
+        /// Moves the Enumerator to the previous element in the collection.
         /// </summary>
         /// <returns>
-        /// True if the Enumerator could move.
+        /// true if the Enumerator did move.
         /// </returns>
         public bool MovePrevious();
 
         /// <summary>
-        /// It moves the Enumerator to the next position.
+        /// Moves the Enumerator to the first element in the collection.
         /// </summary>
         /// <returns>
-        /// True if the Enumerator could move.
+        /// true if the Enumerator could move.
         /// </returns>
         public bool MoveFirst();
 
         /// <summary>
-        /// It moves the Enumerator to the last position.
+        /// Moves the Enumerator to the last element in the collection.
         /// </summary>
         /// <returns>
-        /// True if the Enumerator could move.
+        /// true if the Enumerator could move.
         /// </returns>
         public bool MoveLast();
 
         /// <summary>
-        /// It moves the Enumerator beyond the last position indicating a new Record can be added.
+        /// Moves the Enumerator beyond the last element in the collection indicating a new Record can be added.
         /// </summary>
         /// <returns>
-        /// True if the Enumerator could move.
+        /// true if the Enumerator could move.
         /// </returns>
         public bool MoveNew();
 
         /// <summary>
-        /// It moves the Enumerator at a given position in the array.
+        /// Moves the Enumerator to the element at the given position in collection.
         /// </summary>
         /// <param name="index">the zero-based position to move the Enumerator at.</param>
         /// <returns>
-        /// True if the Enumerator could move.
+        /// true if the Enumerator could move.
         /// </returns>
         public bool MoveAt(int index);
 
         /// <summary>
-        /// It finds the record and moves the Enumerator to its position in the array.
+        /// Moves the Enumerator to the given element in collection.
         /// </summary>
         /// <param name="record">A ISQLModel</param>
         /// <returns>
-        /// True if the Enumerator could move.
+        /// true if the Enumerator could move.
         /// </returns>
         public bool MoveAt(ISQLModel record);
 
         /// <summary>
-        /// It tells how many records are in the array.
+        /// Tells how many records are in the collection.
         /// </summary>
-        /// <value>The number of records in the array</value>
+        /// <value>The number of records in collection</value>
         public int RecordCount { get; }
 
     }
--- MyApplication/App.xaml.cs
+++ MyApplication/App.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace MyApplication
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         public App() 
