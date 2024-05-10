ad89aa1198102d7f65740d8b64b62b50f9279eb0
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-10 20:45:40 +0100

    update

--- FrontEnd/Reports/MicrosoftPDFPrinterManager.cs
+++ FrontEnd/Reports/MicrosoftPDFPrinterManager.cs
@@ -12,7 +12,8 @@ namespace FrontEnd.Reports
     }
 
     /// <summary>
-    /// This class interacts with PDFDriverHelper.exe to add and removes PDF Printer's ports.
+    /// This class interacts with PDFDriverHelper.exe which add and removes PDF Printer's ports.
+    /// Then, the <see cref="ManagementScope"/> set the Port as a Default Port that the Printer will use while printing.
     /// <para/>
     /// <c>IMPORTANT:</c>
     /// <para/>
@@ -33,27 +34,40 @@ namespace FrontEnd.Reports
         [DllImport("PrinterPortManager.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         public static extern uint CreateDeletePort(int action, string portName);
 
-        private ConnectionOptions Options() => new()
+        private static ConnectionOptions Options() => new()
         {
             Impersonation = ImpersonationLevel.Impersonate,
             Authentication = AuthenticationLevel.PacketPrivacy,
             EnablePrivileges = true
         };
 
+        /// <summary>
+        /// Connects to the <see cref="ManagementScope"/>. 
+        /// This method is called by <see cref="SetDefaultPort(bool)"/> before it attempts to set the Printer's Port.
+        /// </summary>
         private void Connect()
         {
             scope = new ManagementScope(ManagementPath.DefaultPath, Options());
             scope.Connect();
         }
 
-        private ManagementObjectCollection Collection()
+        /// <summary>
+        /// Gets the PDF Printer.
+        /// </summary>
+        /// <returns>A ManagementObject</returns>
+        private ManagementObject? GetPrinter()
         {
-            SelectQuery oSelectQuery = new SelectQuery(@"SELECT * FROM Win32_Printer WHERE Name = '" + printerName.Replace("\\", "\\\\") + "'");
+            SelectQuery oSelectQuery = new(@"SELECT * FROM Win32_Printer WHERE Name = '" + printerName.Replace("\\", "\\\\") + "'");
             ManagementObjectSearcher oObjectSearcher = new(scope, @oSelectQuery);
-            return oObjectSearcher.Get();
+            return oObjectSearcher.Get().Cast<ManagementObject>().FirstOrDefault();
         }
 
-        public async Task<int> RunPortManagerAsync(PortAction action)
+        /// <summary>
+        /// Executes PrinterPortManager.exe.
+        /// </summary>
+        /// <param name="action">A <see cref="PortAction"/> enum</param>
+        /// <returns>A Task which returns an integer telling the result of PrinterPortManager.exe. If -1, something went wrong.</returns>
+        private async Task<int> RunPortManagerAsync(PortAction action)
         {
             ProcessStartInfo StartInfo = new()
             {
@@ -74,12 +88,20 @@ namespace FrontEnd.Reports
             return process.ExitCode;
         }
 
+        /// <summary>
+        /// Sets the Default Port to PORTPROMPT: and executes PrinterPortManager.exe which deletes the Newly created Port
+        /// </summary>
+        /// <returns>A Task which returns an integer telling the result of PrinterPortManager.exe. If -1, something went wrong.</returns>
         public async Task<int> ResetPort()
         {
             SetDefaultPort(true);
             return await RunPortManagerAsync(PortAction.REMOVE);
         }
 
+        /// <summary>
+        /// Executes PrinterPortManager.exe which create a New Port, then the <see cref="ManagementObject"/> sets it as a Default Port.
+        /// </summary>
+        /// <returns>A Task which returns an integer telling the result of PrinterPortManager.exe. If -1, something went wrong.</returns>
         public async Task<int> SetPort()
         {
             int result = await RunPortManagerAsync(PortAction.ADD);
@@ -87,16 +109,18 @@ namespace FrontEnd.Reports
             return result;
         }
 
+
+        /// <summary>
+        /// Sets the default Printer's Port.
+        /// </summary>
+        /// <param name="useOriginal">true if the default port should be PORTPROMPT:</param>
+        /// <exception cref="Exception">Throw an exception if the Printer was not found.</exception>
         private void SetDefaultPort(bool useOriginal = false)
         {
             Connect();
-            var collection = Collection();
-
-            foreach (ManagementObject oItem in collection)
-            {
-                oItem.Properties["PortName"].Value = (useOriginal) ? originalPort : FilePath;
-                oItem.Put();
-            }
+            ManagementObject? printer = GetPrinter() ?? throw new Exception("Printer not found!");
+            printer.Properties["PortName"].Value = (useOriginal) ? originalPort : FilePath;
+            printer.Put();
         }
         
         public void OpenFile()
