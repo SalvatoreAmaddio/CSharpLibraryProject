3ef2cf85cea0bee0b85e5d93d05e749da5b9a8ea
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-12 16:31:15 +0100

    update

--- FrontEnd/Controller/CMD.cs
+++ FrontEnd/Controller/CMD.cs
@@ -3,6 +3,10 @@ using System.Windows.Input;
 
 namespace FrontEnd.Controller
 {
+    /// <summary>
+    /// This class implements <see cref="ICommand"/>
+    /// </summary>
+    /// <param name="execute">An Action</param>
     public class CMD(Action execute) : ICommand
     {
         public event EventHandler? CanExecuteChanged;
@@ -19,6 +23,11 @@ namespace FrontEnd.Controller
         }
     }
 
+    /// <summary>
+    /// This class implements <see cref="ICommand"/> and deal with click events that take a <see cref="AbstractModel"/> object as argument
+    /// </summary>
+    /// <typeparam name="M">An <see cref="AbstractModel"/> object</typeparam>
+    /// <param name="execute">An <see cref="Action{T}"/></param>
     public class CMD<M>(Action<M?> execute) : ICommand where M : AbstractModel, new()
     {
         public event EventHandler? CanExecuteChanged;
