734e0d56753f1b58ff50a3d37edcd61002ce5198
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-16 20:16:39 +0100

    update

--- FrontEnd/Forms/Controls/GridRows.cs
+++ FrontEnd/Forms/Controls/GridRows.cs
@@ -3,11 +3,11 @@ using System.Windows.Controls;
 
 namespace FrontEnd.Forms
 {
-        /// <summary>
-        /// This class is a short-hand property to set Grid's Row and Column Definitions.
-        /// </summary>
-        public static class Definition 
-        {
+    /// <summary>
+    /// This class is a short-hand property to set Grid's Row and Column Definitions.
+    /// </summary>
+    public static class Definition
+    {
 
         public static readonly DependencyProperty RowDefinitionsProperty =
             DependencyProperty.RegisterAttached("Rows", typeof(string), typeof(Definition),
@@ -24,34 +24,32 @@ namespace FrontEnd.Forms
 
         private static void OnDefinitionsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-                
-                if (d is Grid grid)
+            if (d is Grid grid)
+            {
+                grid.OnApplyTemplate();
+                bool isRow = e.Property.Name.Equals("Rows");
+                string? definitions = e.NewValue as string;
+
+                if (grid.Name.Equals("listHeader") && !isRow)
+                    definitions = $"24,{definitions}";
+
+                if (!string.IsNullOrEmpty(definitions))
                 {
-                    grid.OnApplyTemplate();
-                    bool isRow = e.Property.Name.Equals("Rows");
-                    string? definitions = e.NewValue as string;
+                    if (isRow) grid.RowDefinitions.Clear();
+                    else grid.ColumnDefinitions.Clear();
 
-                    if (grid.Name.Equals("listHeader") && !isRow) 
+                    string[] parts = definitions.Split(',');
+
+                    foreach (string part in parts)
                     {
-                        definitions = $"24,{definitions}";
-                    }
-                    if (!string.IsNullOrEmpty(definitions))
-                    {
-                        if (isRow) grid.RowDefinitions.Clear();
-                        else grid.ColumnDefinitions.Clear();
-
-                        string[] parts = definitions.Split(',');
-
-                        foreach (string part in parts)
-                        {
-                            if (isRow)
-                                grid.RowDefinitions.Add(new RowDefinition() { Height = new(Convert.ToDouble(part)) });
-                            else
-                                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new(Convert.ToDouble(part)) });
-                        }
+                        if (isRow)
+                            grid.RowDefinitions.Add(new RowDefinition() { Height = new(Convert.ToDouble(part)) });
+                        else
+                            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new(Convert.ToDouble(part)) });
                     }
                 }
             }
-
         }
+
+    }
 }
