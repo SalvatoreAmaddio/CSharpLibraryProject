acd489fb97ebfd26f2b72f01b8e71a18db55abe8
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-18 20:40:18 +0100

    update

---
+++ MyApplication/View/LoginForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MyApplication.View
+{
+    /// <summary>
+    /// Interaction logic for LoginForm.xaml
+    /// </summary>
+    public partial class LoginForm : Window
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Psw.TextInput += Psw_TextInput;
+            MessageBox.Show(Psw.Password);
+        }
+
+        private void Psw_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            MessageBox.Show("Hey");
+        }
+    }
+}
