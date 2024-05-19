04d1577e0bad5e5bec33a0ec00c9d3039a1c5bcb
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-19 01:44:02 +0100

    update

---
+++ FrontEnd/Forms/Walkthrough.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FrontEnd.Forms
+{
+    public class Walkthrough : ContentControl
+    {
+        static Walkthrough()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Walkthrough), new FrameworkPropertyMetadata(typeof(Walkthrough)));
+        }
+    }
+}
