04def46a8cec2a2c71d25361675332cdd72c4a4d
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-20 15:18:03 +0100

    update

--- Backend/Database/SQLiteDatabase.cs
+++ Backend/Database/SQLiteDatabase.cs
@@ -4,6 +4,7 @@ using System.Data.Common;
 using System.Data.SQLite;
 using System.Data;
 using System.Reflection;
+using Backend.Exceptions;
 
 namespace Backend.Database
 {
@@ -19,19 +20,29 @@ namespace Backend.Database
         protected override string LastIDQry() => "SELECT last_insert_rowid()";
 
         /// <summary>
+        /// Attempts to create an instance of IDBConnection through the assembly.            
+        /// </summary>
+        /// <returns>A DbConnection object. If null, it is assumed the application is not published as single file.</returns>
+        /// <exception cref="LoadedAssemblyFailure"></exception>
+        private DbConnection? Connector() 
+        {
+            Assembly? assembly = Sys.LoadedDLL.FirstOrDefault(s => s.Name.Equals("System.Data.SQLite"))?.Assembly;
+            if (assembly == null) return null;
+            IDbConnection? connection = (IDbConnection?)assembly.CreateInstance("System.Data.SQLite.SQLiteConnection") ?? throw new LoadedAssemblyFailure();
+            connection.ConnectionString = ConnectionString();
+            return (DbConnection)connection;
+        }
+
+        /// <summary>
         /// If the software is published as SingleFile, the SQLite connection must be established through external Assembly.
         /// On Application' startup, it is important that you call <see cref="Sys.LoadEmbeddedDll(string)"/> by passing the string "System.Data.SQLite" as its argument.
         /// </summary>
         /// <returns>A DbConnection object</returns>
-        /// <exception cref="Exception">Throw an exception</exception>
         public override DbConnection Connect()
         {
-            Assembly? assembly = Sys.LoadedDLL.FirstOrDefault(s => s.Name.Equals("System.Data.SQLite"))?.Assembly;
-            if (assembly == null) return new SQLiteConnection(ConnectionString()); //if the assembly is null, it is assumed the application is not published as single file.
-            //Attempt to create an instance of IDBConnection through the assembly.            
-            IDbConnection ? connection = (IDbConnection?)assembly.CreateInstance("System.Data.SQLite.SQLiteConnection") ?? throw new Exception("Failed to create a connection object from LoadedAssembly");
-            connection.ConnectionString = ConnectionString(); //set the connection string.
-            return (DbConnection)connection;
+            DbConnection? connection = Connector();
+            if (connection == null) return new SQLiteConnection(ConnectionString());
+            return connection;
         }
     }
 }
--- Backend/Exceptions/CustomExceptions.cs
+++ Backend/Exceptions/CustomExceptions.cs
@@ -3,6 +3,6 @@ namespace Backend.Exceptions
 {
     public class NoModelException() : Exception("No Model is set") { }
     public class NoNavigatorException() : Exception("No Navigator is set") { }
-
+    public class LoadedAssemblyFailure() : Exception("Failed to create a connection object from LoadedAssembly") { }
     public class CurrentUserNotSetException() : Exception("CurrentUser.Is property was not set.") { }
 }
