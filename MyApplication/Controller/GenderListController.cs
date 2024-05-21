8ba30e9474c3c1c787735f3acd7b19f9b6145968
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-21 04:23:03 +0100

    update

--- MyApplication/Controller/GenderListController.cs
+++ MyApplication/Controller/GenderListController.cs
@@ -1,26 +1,40 @@
 ﻿using FrontEnd.Controller;
+using FrontEnd.Events;
 using MyApplication.Model;
 
 namespace MyApplication.Controller
 {
     public class GenderListController : AbstractFormListController<Gender>
     {
-        public override string SearchQry { get; set; } = string.Empty;
+        public override string SearchQry { get; set; } = $"SELECT * FROM {nameof(Gender)} WHERE LOWER(GenderName) LIKE @name";
         public override int DatabaseIndex => 1;
+
+        public GenderListController()
+        {
+            OpenWindowOnNew = false;
+            AfterUpdate += OnAfterUpdate;
+        }
+
+        private async void OnAfterUpdate(object? sender, AfterUpdateArgs e)
+        {
+            if (!e.Is(nameof(Search))) return;
+            await SearchRecordAsync();
+        }
 
         public override void OnOptionFilter()
         {
-            throw new NotImplementedException();
         }
 
-        public override Task SearchRecordAsync()
+        public override async Task SearchRecordAsync()
         {
-            throw new NotImplementedException();
+            QueryBuiler.AddParameter("name", Search.ToLower() + "%");
+            var results = await CreateFromAsyncList(QueryBuiler.Query, QueryBuiler.Params);
+            Source.ReplaceRecords(results);
+            GoFirst();
         }
 
         protected override void Open(Gender? model)
         {
-            throw new NotImplementedException();
         }
     }
 }
---
+++ MyApplication/View/GenderListPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MyApplication.Controller;
+using System.Windows.Controls;
+
+namespace MyApplication.View
+{
+    public partial class GenderListPage : Page
+    {
+        public GenderListPage()
+        {
+            InitializeComponent();
+            DataContext = new GenderListController();
+        }
+    }
+}
