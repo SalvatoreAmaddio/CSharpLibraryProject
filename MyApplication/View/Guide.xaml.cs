ac8d1178f006b6a0fed8334ab7fd98d17e2f16c8
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-22 02:32:15 +0100

    update

--- MyApplication/View/Guide.xaml.cs
+++ MyApplication/View/Guide.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MyApplication.View
 {
@@ -22,6 +11,14 @@ namespace MyApplication.View
         public Guide()
         {
             InitializeComponent();
+        }
+
+        private void OnLinkClicked(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            ProcessStartInfo info = new(e.Uri.AbsoluteUri);
+            info.UseShellExecute = true;
+            Process.Start(info);
+            e.Handled = true;
         }
     }
 }
