8237417ac83a7814418ec8a35d553bfa8142043f
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-05-27 22:46:38 +0100

    update

--- Backend/Office/Range.cs
+++ Backend/Office/Range.cs
@@ -27,20 +27,30 @@ namespace Backend.Office
             rng = wrksheet.get_Range(cell1, cell2);
         }
 
+        /// <summary>
+        /// Instantiates a Range object based on indexes. For Example:
+        /// <code>
+        ///     Range range = new(1, 1, 2, 1); //gets the Range from A1 to B1.
+        /// </code>
+        /// This constructor uses <see cref="ConvertIndexToColumnLetter(int)"/> to convert column's indexes to column's letters.
+        /// </summary>
+        /// <exception cref="ExcelIndexException"></exception>
         public Range(_Worksheet wrksheet, int col1 = 1, int row1=1, int col2 = 1, int row2=1)
         {
-            string cell1 = ConvertIndexToColumnLabel(col1) + row1.ToString();
-            string cell2 = ConvertIndexToColumnLabel(col2) + row2.ToString();
+            if (row1 <= 0 || col1 <= 0 || col2 <= 0 || row2 <= 0) throw new ExcelIndexException();
+            string cell1 = ConvertIndexToColumnLetter(col1) + row1.ToString();
+            string cell2 = ConvertIndexToColumnLetter(col2) + row2.ToString();
             rng = wrksheet.get_Range(cell1, cell2);
         }
 
         /// <summary>
         /// Converts the number provided to the corrisponding Column's Letter. For instance, 1 will return "A" and so on.
+        /// This method is called in <see cref="Range(_Worksheet, int, int, int, int)"/> constructor.
         /// </summary>
         /// <param name="columnNumber"></param>
         /// <returns>A string representing the Column Letter.</returns>
         /// <exception cref="ExcelIndexException"></exception>
-        public static string ConvertIndexToColumnLabel(int columnNumber)
+        public static string ConvertIndexToColumnLetter(int columnNumber)
         {
             if (columnNumber <=0) throw new ExcelIndexException();
             int dividend = columnNumber;
